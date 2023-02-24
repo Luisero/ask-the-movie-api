752b139826d368889585769d0d800c2459f518ff
Luis Henrique <luismoura2005@gmail.com>
2023-02-24 17:53:58 -0300

    FIX(auth) login endpoint should be public

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,6 +4,7 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { LocalStrategy } from './strategies/local.strategy';
 import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
   imports:[UserModule, JwtModule.register({
@@ -12,7 +13,7 @@ import { JwtModule } from '@nestjs/jwt';
       expiresIn:'30d'
     }
   })],
-  providers: [AuthService, LocalStrategy],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
   controllers: [AuthController]
 })
 export class AuthModule {}
